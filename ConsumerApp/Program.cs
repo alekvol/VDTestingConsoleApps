87c359588e8f67b5d59d9cae94b85786dc1671e0
alekvol <sane4st@yandex.ru>
2023-07-05 21:28:44 +0400

    Добавлены Producer и Consumer

---
+++ ConsumerApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig()
+{
+    BootstrapServers = "localhost:9094",
+    GroupId = "Consumer",
+    EnableAutoCommit = false,
+};
+
+using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+{
+    consumer.Subscribe("registrations");
+
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+        Console.WriteLine(consumeResult.Message.Value);
+        consumer.Commit();
+    }
+}
--- ProducerApp/Program.cs
+++ ProducerApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig()
+{
+    BootstrapServers = "localhost:9094",
+    //ClientId = Dns.GetHostName()
+};
+
+Console.WriteLine("Введите сообщение: ");
+
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    while (true)
+    {
+        var input = Console.ReadLine();
+        producer.Produce("registrations", new Message<Null, string>() { Value = input });
+    }
+}
